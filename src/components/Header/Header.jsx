2d092111017939b80d33ec069d9c1f85488ffa58
Krystyna1307 <kristina.dudnik8@gmail.com>
2025-04-03 12:33:04 +0200

    create module.css

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,22 +1,21 @@
 import React from "react";
+import s from "./Header.module.css";
 
 const Header = () => {
   return (
-    <div>
-      <header>
-        <nav>
-          <a href="">RentalCar</a>
-          <ul>
-            <li>
-              <a href="">Home</a>
-            </li>
-            <li>
-              <a href="">Catalog</a>
-            </li>
-          </ul>
-        </nav>
-      </header>
-    </div>
+    <header className={s.wrapper}>
+      <nav>
+        <a href="">RentalCar</a>
+        <ul>
+          <li>
+            <a href="">Home</a>
+          </li>
+          <li>
+            <a href="">Catalog</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
   );
 };
 
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,8 +1,9 @@
 import React from "react";
+import s from "./Home.module.css";
 
 const Home = () => {
   return (
-    <div>
+    <div className={s.wrapper}>
       <h1>Find your perfect rental car</h1>
       <p>Browse our catalog and choose the best car for your needs.</p>
       <button>View Catalog</button>
