17f65652d95956ce2187b375b5db94f4e2e15b52
Krystyna1307 <kristina.dudnik8@gmail.com>
2025-04-03 20:59:36 +0200

    and notFound

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,6 +3,7 @@ import Header from "./Header/Header";
 import HomePage from "../pages/HomePage/HomePage";
 import CatalogPage from "../pages/CatalogPage/CatalogPage";
 import DetailsPage from "../pages/DetailsPage/DetailsPage";
+import NotFound from "../pages/NotFound/NotFound";
 
 const App = () => {
   return (
@@ -12,6 +13,7 @@ const App = () => {
         <Route path="/" element={<HomePage />} />
         <Route path="/catalog" element={<CatalogPage />} />
         <Route path="/catalog/:id" element={<DetailsPage />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </div>
   );
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 import s from "./Header.module.css";
 import logo from "../../assets/logo.svg";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
+import clsx from "clsx";
+
+const buildLinkClass = ({ isActive }) => {
+  return clsx(s.link, isActive && s.active);
+};
 
 const Header = () => {
   return (
@@ -10,8 +15,8 @@ const Header = () => {
         <img src={logo} alt="Logo" width="104" height="16" />
       </a>
       <nav className={s.nav}>
-        <Link to="/">Home</Link>
-        <Link to="/catalog">Catalog</Link>
+        <NavLink className={buildLinkClass} to="/">Home</NavLink>
+        <NavLink className={buildLinkClass} to="/catalog">Catalog</NavLink>
       </nav>
     </header>
   );
