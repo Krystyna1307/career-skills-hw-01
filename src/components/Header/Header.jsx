c1ba66a2cd5486e14da997a84e0a98869d85ebf0
Krystyna1307 <kristina.dudnik8@gmail.com>
2025-04-03 20:29:19 +0200

    add links

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,22 +1,17 @@
 import React from "react";
 import s from "./Header.module.css";
 import logo from "../../assets/logo.svg";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
-    <header className={s.wrapper}>
-      <nav>
-        <a href="/">
-          <img src={logo} alt="Logo" width="104" height="16" />
-        </a>
-        <ul>
-          <li>
-            <a href="/">Home</a>
-          </li>
-          <li>
-            <a href="/">Catalog</a>
-          </li>
-        </ul>
+    <header className={s.header}>
+      <a href="/">
+        <img src={logo} alt="Logo" width="104" height="16" />
+      </a>
+      <nav className={s.nav}>
+        <Link to="/">Home</Link>
+        <Link to="/catalog">Catalog</Link>
       </nav>
     </header>
   );
